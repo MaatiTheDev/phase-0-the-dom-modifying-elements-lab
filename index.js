0d8760dbb5d29285e0a5c7fca81ee32dd5ff3c68
felix maati <maatifelix001@gmail.com>
2024-07-09 09:02:30 +0300

    ceating and incerting

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Felix Maati is the champion';
+
+
+document.body.appendChild(newHeader);
